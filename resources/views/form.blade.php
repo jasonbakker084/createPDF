f59560a50a59f7d54ff6d68387babd938115cf91
Jason Bakker <jasonbakker084@gmail.com>
2020-01-23 22:01:30 +0100

    little bugs

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -42,8 +42,46 @@
                     <br>
                      <button type="submit" class="btn btn-primary">Create PDF</button>
                 </form>
+                <hr>
+                <br>
+                <br>
+                <form class="form-horizontal" action="" method="post" name="uploadCSV"
+                      enctype="multipart/form-data">
+                    <div class="input-row">
+                        <label class="col-md-4 control-label">Choose CSV File</label> <input
+                            type="file" name="file" id="file" accept=".csv">
+                        <button type="submit" id="submit" name="import"
+                                class="btn-submit btn btn-primary">Import</button>
+                        <br />
+                    </div>
+                    <div id="labelError"></div>
+                </form>
             </div>
         </div>
     </div>
 </div>
+<script type="text/javascript">
+    $(document).ready(
+        function() {
+            $("#frmCSVImport").on(
+                "submit",
+                function() {
+
+                    $("#response").attr("class", "");
+                    $("#response").html("");
+                    var fileType = ".csv";
+                    var regex = new RegExp("([a-zA-Z0-9\s_\\.\-:])+("
+                        + fileType + ")$");
+                    if (!regex.test($("#file").val().toLowerCase())) {
+                        $("#response").addClass("error");
+                        $("#response").addClass("display-block");
+                        $("#response").html(
+                            "Invalid File. Upload : <b>" + fileType
+                            + "</b> Files.");
+                        return false;
+                    }
+                    return true;
+                });
+        });
+</script>
 @endsection
